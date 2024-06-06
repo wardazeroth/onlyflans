f70ab268acb14e36908bf0fc2a9029787363a017
Javier Guarda <guardaajavier@gmail.com>
2024-06-05 23:51:13 -0400

    formulario + validaciones + alertas

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from main.views import  acerca, indice, bienvenido
+from main.views import  acerca, indice, bienvenido, contact_form
 
 urlpatterns = [
     path('', indice, name='indice'),
     path('acerca', acerca, name='acerca'),
     path('bienvenido', bienvenido, name='bienvenido'),
+    path('contact_form', contact_form, name='contact_form')
+    
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from main.pasteles import flanes
 
 # Create your views here.
@@ -15,6 +15,33 @@ def acerca(req):
 
 def bienvenido(req):
     context = {
-        'cliente': 'Mente Maestra'
+        'cliente': 'Elmo Lesto'
     }
     return render(req, 'welcome.html', context)
+
+def contact_form(req):
+    errores = []
+    customer_name = req.POST['customer_name']
+    customer_email = req.POST['customer_email']
+    message = req.POST['message']
+    
+    if len(customer_name) > 64:
+        errores.append('Su nombre excede el máximo permitido')     
+        
+    if not '@' in customer_email:
+        errores.append('Coloca @ en tu email')
+        
+    context = {'mensaje_error': errores}
+                       
+    if len(errores) > 0:
+        return render(req, 'welcome.html', context)
+    else:
+        return render(req, 'yeah.html', context)
+      
+    
+    
+    #Ahora tengo que validar que customer_email tenga al menos 1 arroba y que customer_name  sea de largo maximo 64
+    
+    #si len(errores) == 0: redirijo a pagina de exito
+    #si len(errores) > 0: vuelvo a cargar 'welcome.html' pero ahora mostrando los errores. 
+    
